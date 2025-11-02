4906a139700525c88dcf88a9f6c070e4f9a63f97
zhepingjiang <zheping.jiang@gmail.com>
2025-11-02 13:06:12 -0500

    Initial commit - FE and BE setup

---
+++ pms-backend/src/main/java/com/laioffer/pmsbackend/PmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laioffer.pmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsBackendApplication.class, args);
+    }
+
+}
