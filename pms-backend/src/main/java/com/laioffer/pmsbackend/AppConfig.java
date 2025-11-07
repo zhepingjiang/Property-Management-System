e81898241d81b92b0b0e9dcff1de927b7f88984f
SiteLi <nadaein937@gmail.com>
2025-11-06 20:11:29 -0600

    test by SiteLi

--- pms-backend/src/main/java/com/laioffer/pmsbackend/AppConfig.java
+++ pms-backend/src/main/java/com/laioffer/pmsbackend/AppConfig.java
@@ -1,5 +1,5 @@
 package com.laioffer.pmsbackend;
-
+//ssss
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
