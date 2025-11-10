5c740c2952c30b73509bb3cfa8c57c7b4098baa2
Zheping Jiang <zheping.jiang@gmail.com>
2025-11-10 00:46:45 -0500

    Add MUI (Material UI and Tailwind CSS dependencies

---
+++ pms-frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 让 Tailwind 扫描所有源文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
